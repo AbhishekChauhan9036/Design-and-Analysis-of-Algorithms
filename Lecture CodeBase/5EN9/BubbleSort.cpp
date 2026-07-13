02dcd30be176ccad1f30231185277c8d8257db6c
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-07-13 12:07:30 +0530

    DAA

--- Lecture CodeBase/5EN9/BubbleSort.cpp
+++ Lecture CodeBase/5EN9/BubbleSort.cpp
@@ -9,7 +9,6 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
@@ -19,7 +18,6 @@ int main(){
             }
         }
     }
-
     cout<<"Sorted Array: "<<endl;
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
