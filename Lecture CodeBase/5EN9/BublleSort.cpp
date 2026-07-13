830247c2f27d66c0d5b56c218c00358463b1373b
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-07-13 11:42:51 +0530

    DAA

---
+++ Lecture CodeBase/5EN9/BublleSort.cpp
@@ -0,0 +1,33 @@
+#include"bits/stdc++.h"
+using namespace std;
+int main(){
+    int n;
+    cout<<"Please enter the size of Array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Please enter the Elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1]; 
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    cout<<"Sorted Array: "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
+
+/**
+ * TC : O(n)^2
+ * SC : O(1)
+ */
