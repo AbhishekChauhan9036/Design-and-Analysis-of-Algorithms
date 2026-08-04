49b86a5a544a5130f23e1d1a71e6b700a9231d34
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-08-04 10:15:26 +0530

    DAA

---
+++ Lecture CodeBase/5EN9/CoinChangeWays.py
@@ -0,0 +1,21 @@
+# https://www.geeksforgeeks.org/problems/coin-change2448/1
+
+
+class Solution:
+    def count(self, coins, sum):
+        # dp[i] = Number of ways to make amount i
+        dp = [0] * (sum + 1)
+        dp[0] = 1  # One way to make sum 0
+
+        # Process each coin
+        for coin in coins:
+            for amount in range(coin, sum + 1):
+                dp[amount] += dp[amount - coin]
+
+        return dp[sum]
+        
+        
+'''
+TC : O(n × sum)
+SC : O(sum)
+'''
