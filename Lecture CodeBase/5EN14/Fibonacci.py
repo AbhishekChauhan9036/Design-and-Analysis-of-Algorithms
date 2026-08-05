85ef7654d758b046f22a757ef41e4803ba0fa3c6
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2026-08-05 18:23:44 +0530

    DAA

---
+++ Lecture CodeBase/5EN14/Fibonacci.py
@@ -0,0 +1,29 @@
+def fib(n):
+    if n==0 or n==1:
+        return n
+    return fib(n-1)+fib(n-2)
+
+ans = fib(6)
+print(ans)
+'''
+TC : 2^n
+SC : O(log n)
+'''
+
+
+def fun(n):
+    if n==0 or n==1:
+        return n
+
+    dp = [0]*(n+1)
+    dp[0], dp[1] = 0, 1
+    for i in range(2, n+1):
+        dp[i] = dp[i-1]+dp[i-2]
+    return dp[n]
+
+ans = fun(6)
+print(ans)
+'''
+TC : O(n)
+SC : O(n)
+'''
